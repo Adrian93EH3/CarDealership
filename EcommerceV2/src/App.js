c430e0b1f54f21b9cb982930aaf7ba1bf462be2a
Adrian Daniel Diaz <Adrian@MacBook-Pro-2.attlocal.net>
2020-06-11 15:05:48 -0400

    Update to code/Components

--- EcommerceV2/src/App.js
+++ EcommerceV2/src/App.js
@@ -2,61 +2,23 @@ import React from 'react';
 import './App.css';
 import Inventory from './Inventory/inventory.js'
 import Contact from './Contact/contact.js'
-import SlideShow from './Slider/slider';
+import Nav from './Navbar/navbar.js'
+import Footer from './Footer/footer.js'
+import Home from './Home/home.js'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    if (window.location.pathname === '/inventory.html') {
-      return (<Inventory/>);
-    } else if (window.location.pathname === '/contact.html'){
-      return(<Contact/>);
-    } else {
-      console.log("This is the home page!");
-    }
     return (
-      <React.Fragment>
-        <header className="header">
-        <img className="logo" src="Cars/logo.jpeg" alt="logo" /><a href=""></a><a href=""></a>
-          <ul className="nav">
-            <li><a className="current" href="#">Home</a></li>
-            <li><a href="inventory.html">Inventory</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </header>
-        <SlideShow/>	
-        <h1 className="featureItems">Featured Vehicles</h1>
-        <div className="feature">
-          <article className="child">
-            <img className="child--image" src="Cars/evo.jpg" alt="picture_of_1993_evolution_II"/>
-            <h4> 1993 Mitsubishi Evolution II </h4>
-            <p> This bright yellow Evo III comes bone stock. With a whopping 266whp and a tremendous AWD system, this car is rowdy. Exterior and Interior 10/10 with 35K verified miles.</p>
-            <h5 className="price">Offer: $30,000</h5>
-          </article>
-          <article className="child">
-            <img className="child--image" src="Cars/supra2.jpg" alt="picture_of_2019_supra_rz"/>
-            <h4> 1993 Toyota Supra RZ </h4>
-            <p> This Supra comes tastefully modified and well maintained. Drives hard and runs perfectly. Exterior and Interior 10/10 with 55K verified chassis miles Engine has been swapped.</p>
-            <h5 className="price">Offer: $70,000</h5>
-          </article>
-          <article className="child">
-            <img className="child--image" src="Cars/supra.jpg" alt="picture_of_1993_supra_sz"/>
-            <h4> 1993 Toyota Supra SZ </h4>
-            <p> Lightly modified Supra, with a gorgeous Midnight Purple III color change. Paint is brand new, interior is immaculate and exterior has a few dings. This Supra has 62K verified miles.</p>
-            <h5 className="price"> Offer: $45,000</h5>
-          </article>
-        </div>
-        <footer className="footer1">
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-          <ul className="ul-2">
-            <li><a className="facebook" href="http://facebook.com/" target="_blank"><i className="fa fa-fw fa-2x fa-inverse fa-facebook"></i></a></li>
-            <li><a className="twitter" href="http://twitter.com/" target="_blank"><i className="fa fa-fw fa-2x fa-inverse fa-twitter"></i></a></li>
-            <li><a className="instagram" href="https://www.instagram.com/" target="_blank"><i className="fa fa-fw fa-2x fa-inverse fa-instagram"></i></a></li>
-          </ul>
-          <a className="a" href="#" id="footer">
-          </a>
-          <h3> &copy;2020 || Adrian D. Diaz</h3>
-        </footer>
-      </React.Fragment>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/inventory" component={Inventory}/>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
+        <Footer></Footer>
+        </BrowserRouter>
     );
   }
 }
--- EcommerceV2/src/Contact/contact.js
+++ EcommerceV2/src/Contact/contact.js
@@ -1,108 +1,34 @@
 import React from "react";
 import SlideShow from "../Slider/slider2";
-const defaultState = {
-  name: "",
-  email: "",
-  message: "",
-  nameError: "",
-  emailError: "",
-};
 
-class Form extends React.Component {
-  state = defaultState;
-
-  handleChange = (event) => {
-    const isCheckbox = event.target.type === "checkbox";
-    this.setState({
-      [event.target.name]: isCheckbox
-        ? event.target.checked
-        : event.target.value,
-    });
-  };
-
-  validate = () => {
-    let nameError = "";
-    let emailError = "";
-
-    if (!this.state.name) {
-      nameError = "Please fill out your name.";
-    }
-
-    if (!this.state.email.includes("@")) {
-      emailError = "This email is not valid.";
-    }
-
-    if (nameError || emailError) {
-      this.setState({ nameError, emailError });
-      return false;
-    }
-
-    return true;
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const isValid = this.validate();
-    if (isValid) {
-      console.log(this.state);
-      //Clearing form
-    this.setState(defaultState);
-    }
-  };
-  render() {
-    return (
-      <React.Fragment>
+const Contact = () => {
+return (
       <div>
-        <header className="header">
-                    <img className="logo" src="Cars/logo.jpeg" alt="logo" /><a href=""></a>
-                    <ul className="nav">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="inventory.html">Inventory</a></li>
-                        <li><a className="current" href="#">Contact</a></li>
-                    </ul>
-                </header>
-                <SlideShow/>
-                <img className="hidden" src="Cars/index.jpg" alt="hidden" />
-                <div className="showcase container">
-            <h2 className="text-primary">Contact Us</h2>
-            <form onSubmit={this.handleSubmit} class="container">
+        <SlideShow />
+        <div className="showcase container">
+          <h2 className="text-primary">Contact Us</h2>
+          <form class="container">
 
-<div class="form-group">
-    <label for="name">Name: </label>
-    <input placeholder="'First Name' 'Last Name'" type="text" id="name" name="name" value={this.state.name}
-    onChange={this.handleChange} required/>
-    <div className="validate">{this.state.nameError}</div>
-</div>
+            <div class="form-group">
+              <label for="name">Name: </label>
+              <input placeholder="'First Name' 'Last Name'" type="text" id="name" name="name" required />
+            </div>
 
-<div class="form-group">
-    <label for="email">Email: </label>
-    <input placeholder="youremailhere@email.com" type="text" id="email" name="email" value={this.state.email}
-    onChange={this.handleChange} required/>
-    <div className="validate">{this.state.emailError}</div>
-</div>
+            <div class="form-group">
+              <label for="email">Email: </label>
+              <input placeholder="youremailhere@email.com" type="email" id="email" name="email" required />
+            </div>
 
-<div class="form-group">
-    <label for="message">Message: </label>
-    <textarea placeholder="Did one of our vehicles catch your eye? Please leave us your email, a phone number to contact you and the vehicle make and model that you are interested in! Our team is ready to respond within 12 hours and set up a meeting with you swiftly! Please feel free to also provide us with any feedback you may have!" id="message" name="message"></textarea>
-</div>
-<button type="submit" class="btn"> Submit </button>
-</form>
-          </div>
+            <div class="form-group">
+              <label for="message">Message: </label>
+              <textarea placeholder="Did one of our vehicles catch your eye? Please leave us your email, a phone number to contact you and the vehicle make and model that you are interested in! Our team is ready to respond within 12 hours and set up a meeting with you swiftly! Please feel free to also provide us with any feedback you may have!" id="message" name="message"></textarea>
+            </div>
+            <button type="submit" class="btn"> Submit </button>
+          </form>
+        </div>
       </div>
-                          <footer className="footer1">
-                          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-                          <ul className="ul-2">
-                            <li><a className="facebook" href="http://facebook.com/" src="https://image.flaticon.com/icons/svg/254/254390.svg" alt="logo_of_facebook" target="_blank"><i className="fa fa-fw fa-2x fa-inverse fa-facebook"></i></a></li>
-                            <li><a className="twitter" href="http://twitter.com/"><i className="fa fa-fw fa-2x fa-inverse fa-twitter"></i></a></li>
-                            <li><a className="instagram" href="https://www.instagram.com/"><i className="fa fa-fw fa-2x fa-inverse fa-instagram"></i></a></li>
-                          </ul>
-                          <a className="a" href="#" id="footer">
-                          </a>
-                          <h3 className="copyright"> &copy;2020 || Adrian D. Diaz</h3>
-                        </footer>
-                            </React.Fragment>
+
     );
   }
-}
 
-export default Form;
+export default Contact;
