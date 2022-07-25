3644ec24ffe1cd9061335f93a00668f82542fa9d
Adrian Diaz <adrian93eh3@gmail.com>
2022-07-25 11:27:28 -0400

    working on dynamically changing the navbar class

--- EcommerceV2/src/Navbar/navbar.js
+++ EcommerceV2/src/Navbar/navbar.js
@@ -4,13 +4,21 @@ import {Link} from "react-router-dom";
 
 const Nav = () => {
 
+  const handleClick = event => {
+    // 👇️ toggle class on click
+    event.currentTarget.classList.toggle('current');
+    
+    // 👇️ remove class on click
+      event.currentTarget.classList.remove('current');
+  };
+
     return (
       <header className="header">
         <img className="logo" src="Cars/logo.jpeg" alt="logo" /><a href=""></a><a href=""></a>
           <ul className="nav">
-            <li><Link className="current" to="/">Home</Link></li>
-            <li><Link to="/inventory">Inventory</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link onClick={handleClick} to="/">Home</Link></li>
+            <li><Link onClick={handleClick} to="/inventory">Inventory</Link></li>
+            <li><Link onClick={handleClick} to="/contact">Contact</Link></li>
           </ul>
         </header>       
     );
